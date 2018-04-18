cf72330d3cd02ee598a5cfcab46f26c2a0ecc96d
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-04-18 16:19:39 -0700

    added files completed

---
+++ public/app.js
@@ -0,0 +1,43 @@
+// TODO:
+
+  // 1. Make a reusable function for creating a table body in index.html with the results from your MongoDB query
+  // Each row should have info for one animal.
+
+  function getTableData(table) {
+    var data = [];
+    table.find('tr').each(function (rowIndex, r) {
+        var cols = [];
+        $(this).find('th').each(function (colIndex, c) {
+            cols.push(c.textContent);
+        });
+        data.push(cols);
+    });
+    return data;
+}
+
+  // 2. Make two AJAX functions that fire when users click the two buttons on index.html.
+  //     a. When the user clicks the Weight button, the table should display the animal data sorted by weight.
+  $("#weight-sort").on("click", function(){
+
+  });
+  //     b. When the user clicks the Name button, the table should display the animal data sorted by name.
+
+  // Good luck!
+
+  // *Hint*: We don't want to keep adding to the table with each button click. We only want to show the new results.
+  // What can we do to the table to accomplish this?
+
+  // *Bonus*: Write code to set an 'active' state on the column header. It should make the color sorted-by column red.
+  // *Bonus*: Add additional ways to sort (e.g. by class or number of legs)
+
+
+// We'll be rewriting the table's data frequently, so let's make our code more DRY
+// by writing a function that takes in data (JSON) and creates a table body
+function displayResults(data) {
+  // Add to the table here...
+}
+
+$.getJSON("/all", function(data) {
+  // Call our function to generate a table body
+  displayResults(data);
+});
